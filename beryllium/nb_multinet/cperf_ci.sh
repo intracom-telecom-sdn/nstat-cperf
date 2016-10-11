fe3ceb4b1f90b867d546974226ab3ab0fd4ffd90
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-10-11 15:09:44 +0300

    Correct python3.4 $NSTAT_WORKSPACE/stress_test/nstat_orchestrator.py

--- beryllium/nb_multinet/cperf_ci.sh
+++ beryllium/nb_multinet/cperf_ci.sh
@@ -24,7 +24,7 @@ echo '-------------------------------------------------------------------------'
 
 docker-compose up -d
 
-for container_id in $(docker ps | awk 'FNR > 1 {print $NF}')
+for container_id in nstat controller nb-gen mn-01 mn-02
 do
     docker exec -i $container_id /bin/bash -c "rm -rf $NSTAT_WORKSPACE && \
         cd /opt && \
@@ -42,7 +42,7 @@ python3.4 $NSTAT_WORKSPACE/stress_test/nstat_orchestrator.py \
      --test=$TEST_TYPE \
      --ctrl-base-dir=$NSTAT_WORKSPACE/controllers/odl_beryllium_pb/ \
      --sb-generator-base-dir=$NSTAT_WORKSPACE/emulators/multinet/ \
-     --nb-generator-base-dir=$NSTAT_WORKSPACE/emulators/nb_generator/
+     --nb-generator-base-dir=$NSTAT_WORKSPACE/emulators/nb_generator \
      --json-config=$NSTAT_WORKSPACE/$CONFIG_FILENAME.json \
      --json-output=$NSTAT_WORKSPACE/${CONFIG_FILENAME}_results.json \
      --html-report=$NSTAT_WORKSPACE/report.html \
