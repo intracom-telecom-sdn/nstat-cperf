88e541d7bf541c197f58749bb9a4c16cd8868941
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-10-19 16:59:57 +0300

    Removed exit command from cperf_ci.sh

--- boron_stress/sb_mtcbench/cperf_ci.sh
+++ boron_stress/sb_mtcbench/cperf_ci.sh
@@ -23,7 +23,7 @@ echo '-------------------------------------------------------------------------'
 echo 'TEST TYPE      : '$TEST_TYPE
 echo 'CONFIG_FILENAME: '$CONFIG_FILENAME
 echo '-------------------------------------------------------------------------'
-exit
+
 docker-compose up -d
 
 for container_id in nstat controller mtcbench
