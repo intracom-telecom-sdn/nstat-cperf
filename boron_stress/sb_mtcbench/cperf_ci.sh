f92ada04f9602ac41676dc8f4faadd904283629e
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-10-28 20:45:23 +0300

    Adaptations for sb_idle_scalability_mtcbench

--- boron_stress/sb_mtcbench/cperf_ci.sh
+++ boron_stress/sb_mtcbench/cperf_ci.sh
@@ -16,14 +16,14 @@ NSTAT_WORKSPACE=/opt/nstat
 RESULTS_DIR=$CONFIG_FILENAME"_results"
 
 TMP=${CONFIG_FILENAME#*_*_}
-#TEST_TYPE=${TMP%.*}
-TEST_TYPE=${TMP%_*_*_*_*}
+TEST_TYPE=${TMP%.*}
+#TEST_TYPE=${TMP%_*_*_*_*}
 
 echo '-------------------------------------------------------------------------'
 echo 'TEST TYPE      : '$TEST_TYPE
 echo 'CONFIG_FILENAME: '$CONFIG_FILENAME
 echo '-------------------------------------------------------------------------'
-
+exit
 docker-compose up -d
 
 for container_id in nstat controller mtcbench
